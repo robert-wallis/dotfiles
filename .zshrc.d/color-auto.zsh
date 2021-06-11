906452f17c613351e31a2f57dac9a76bb7454c12
Robert Wallis <smilingrob@gmail.com>
2021-06-11 16:46:29 -0700

    Aliases that include color, including ip and cool stuff.

---
+++ .zshrc.d/color-auto.zsh
@@ -0,0 +1,8 @@
+# https://wiki.archlinux.org/title/Color_output_in_console
+alias diff='diff --color=auto'
+alias grep='grep --color=auto'
+alias ip='ip --color=auto'
+export LESS='-R --use-color -Dd+r$Du+b'
+alias dmesg='dmesg --color=always'
+alias ls='ls --color=auto'
+export MANPAGER='less -R --use-color -Dd+r -Du+b'
