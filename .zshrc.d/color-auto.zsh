7860c2eccbb60a0f67f1e0c2e7ca2ae7484455ad
Robert Wallis <smilingrob@gmail.com>
2021-06-11 17:00:45 -0700

    colors, and executable

--- .zshrc.d/color-auto.zsh
+++
@@ -1,8 +0,0 @@
-# https://wiki.archlinux.org/title/Color_output_in_console
-alias diff='diff --color=auto'
-alias grep='grep --color=auto'
-alias ip='ip --color=auto'
-export LESS='-R --use-color -Dd+r$Du+b'
-alias dmesg='dmesg --color=always'
-alias ls='ls --color=auto'
-export MANPAGER='less -R --use-color -Dd+r -Du+b'
