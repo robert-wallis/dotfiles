cd083589bab0dc4cdf6305ced9c9d4c272e62d2e
Robert Wallis <smilingrob@gmail.com>
2021-06-21 10:54:14 -0700

    more case statements for library paths

--- .zshrc.d/LIBRARY_PATH.zsh
+++ .zshrc.d/LIBRARY_PATH.zsh
@@ -1,14 +1,17 @@
 # SDL
 USR_LOCAL_LIB="/usr/local/lib"
 if [ -d $USR_LOCAL_LIB ]; then
-    export USR_LOCAL_LIB
-	if [ -z "$LIBRARY_PATH" ]
-	then
-		export LIBRARY_PATH=$USR_LOCAL_LIB
+	if [[ "$LIBRARY_PATH" == *"$USR_LOCAL_LIB"* ]]; then
 		printf "LIBRARY_PATH        $LIBRARY_PATH\n"
 	else
-		export LIBRARY_PATH="$LIBRARY_PATH:$USR_LOCAL_LIB"
-		printf "LIBRARY_PATH        $LIBRARY_PATH\n"
+		if [ -z "$LIBRARY_PATH" ]
+		then
+			export LIBRARY_PATH=$USR_LOCAL_LIB
+			printf "LIBRARY_PATH        $LIBRARY_PATH\n"
+		else
+			export LIBRARY_PATH="$LIBRARY_PATH:$USR_LOCAL_LIB"
+			printf "LIBRARY_PATH        $LIBRARY_PATH\n"
+		fi
 	fi
 fi
 
--- .zshrc.d/libffi.zsh
+++ .zshrc.d/libffi.zsh
@@ -1,7 +1,14 @@
 # Needed to use Gtk+ in Rust.
 LIBFFI_HOME="/usr/local/opt/libffi"
 LIBFFI_PKGCONFIG="$LIBFFI_HOME/lib/pkgconfig"
-if [[ -d $LIBFFI_PKGCONFIG && "$PKG_CONFIG_PATH" != *"$LIBFFI_PKGCONFIG"* ]]; then
-    export PKG_CONFIG_PATH="$LIBFFI_PKGCONFIG:$PKG_CONFIG_PATH"
-    printf "PKG_CONFIG_PATH     $PKG_CONFIG_PATH\n"
+if [ -d $LIBFFI_PKGCONFIG ]; then
+	if [[ "$PKG_CONFIG_PATH" == *"$LIBFFI_PKGCONFIG"* ]]; then
+		printf "PKG_CONFIG_PATH     $PKG_CONFIG_PATH\n"
+	elif [ -z "$PKG_CONFIG_PATH" ]; then
+		export PKG_CONFIG_PATH=$LIBFFI_PKGCONFIG
+		printf "PKG_CONFIG_PATH     $PKG_CONFIG_PATH\n"
+	else
+		export PKG_CONFIG_PATH="$LIBFFI_PKGCONFIG:$PKG_CONFIG_PATH"
+		printf "PKG_CONFIG_PATH     $PKG_CONFIG_PATH\n"
+	fi
 fi
