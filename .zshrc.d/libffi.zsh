761752bf9a92b894d591656c4ebd6d65365d19b9
Robert Wallis <smilingrob@gmail.com>
2021-06-21 10:36:33 -0700

    only add to pkgconfig if it's not already there

--- .zshrc.d/libffi.zsh
+++ .zshrc.d/libffi.zsh
@@ -1,6 +1,7 @@
 # Needed to use Gtk+ in Rust.
 LIBFFI_HOME="/usr/local/opt/libffi"
-if [ -d $LIBFFI_HOME ]; then
-    export PKG_CONFIG_PATH="$LIBFFI_HOME/lib/pkgconfig:$PKG_CONFIG_PATH"
+LIBFFI_PKGCONFIG="$LIBFFI_HOME/lib/pkgconfig"
+if [[ -d $LIBFFI_PKGCONFIG && "$PKG_CONFIG_PATH" != *"$LIBFFI_PKGCONFIG"* ]]; then
+    export PKG_CONFIG_PATH="$LIBFFI_PKGCONFIG:$PKG_CONFIG_PATH"
     printf "PKG_CONFIG_PATH     $PKG_CONFIG_PATH\n"
 fi
