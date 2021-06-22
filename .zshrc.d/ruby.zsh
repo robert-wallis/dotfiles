f5738474771f5baf0fe67b7e95730523257af481
Robert Wallis <smilingrob@gmail.com>
2021-06-21 20:02:05 -0700

    Changed order of rvm and rbenv

--- .zshrc.d/ruby.zsh
+++ .zshrc.d/ruby.zsh
@@ -1,16 +1,26 @@
 # Ruby
-# rbenv
-if which rbenv > /dev/null; then
-    printf "rbenv"
-	eval "$(rbenv init - /bin/zsh)"
-	printf "               $(which ruby)\n"
-fi
 
+# rvm
 rvm_bin="$HOME/.rvm/bin"
 if [ -d $rvm_bin ]; then
 	printf "rvm                 $rvm_bin\n"
 	if [[ "$PATH" == *"$rvm_bin"* ]]; then
-		export PATH="$PATH:$HOME/.rvm/bin"
 	else
+		export PATH="$PATH:$rvm_bin"
 	fi
+fi
+
+# rbenv
+rbenv_bin="$HOME/.rbenv/bin"
+if [ -d "$rbenv_bin" ]; then
+	if [[ "$PATH" == *"$rbenv_bin"* ]]; then
+	else
+		printf "rbenv               $rbenv_bin\n"
+		export PATH="$PATH:$rbenv_bin"
+	fi
+fi
+if which rbenv > /dev/null; then
+    printf "rbenv"
+	eval "$(rbenv init - /bin/zsh)"
+	printf "               $(which ruby)\n"
 fi
