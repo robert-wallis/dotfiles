726151371982fc94d4832ad0456ee046d4144af6
Robert Wallis <smilingrob@gmail.com>
2021-06-21 18:48:54 -0700

    rvm

--- .zshrc.d/ruby.zsh
+++ .zshrc.d/ruby.zsh
@@ -5,3 +5,12 @@ if which rbenv > /dev/null; then
 	eval "$(rbenv init - /bin/zsh)"
 	printf "               $(which ruby)\n"
 fi
+
+rvm_bin="$HOME/.rvm/bin"
+if [ -d $rvm_bin ]; then
+	printf "rvm                 $rvm_bin\n"
+	if [[ "$PATH" == *"$rvm_bin"* ]]; then
+		export PATH="$PATH:$HOME/.rvm/bin"
+	else
+	fi
+fi
