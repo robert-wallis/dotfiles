be93446b4c4a835371fa5119ebf726a7a62a3f7a
Robert Wallis <robert.wallis@zoom.us>
2023-10-26 13:56:24 -0700

    check if the brew executable exists, then eval it

--- .zshrc.d/brew.zsh
+++ .zshrc.d/brew.zsh
@@ -1,4 +1,7 @@
 # Homebrew
+if [ -f "/opt/homebrew/bin/brew" ]; then
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
 if type brew &>/dev/null; then
     FPATH=$(brew --prefix)/share/zsh/site-functions:$FPATH
     printf "brew --prefix       $(brew --prefix)\n"
