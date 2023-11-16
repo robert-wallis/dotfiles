a4173bd2b95fa57d0f3a695ca12c92d1543a3136
Robert Wallis <smilingrob@gmail.com>
2023-11-16 12:12:32 -0800

    Added nvim lsp-zero and tested with rust_analyzer.

---
+++ nvim/after/plugin/lsp.lua
@@ -0,0 +1,24 @@
+local lsp_zero = require('lsp-zero')
+
+lsp_zero.on_attach(function(client, buf)
+	-- see :help lsp-zero-keybindings to learn the available actions
+	lsp_zero.default_keymaps({buffer = buf})
+end)
+
+require("mason").setup({})
+require("mason-lspconfig").setup {
+	ensure_installed = {
+		"eslint",
+		"gopls",
+		"jedi_language_server",
+		"lua_ls",
+		"rust_analyzer",
+		"tsserver",
+		"volar",
+	},
+	handlers = {
+		-- important to have handlers, or lspconfig won't set up correctly
+		lsp_zero.default_setup,
+	},
+}
+
--- nvim/lua/robert/lazy.lua
+++ nvim/lua/robert/lazy.lua
@@ -2,7 +2,18 @@ require("lazy").setup({
 	'nvim-treesitter/nvim-treesitter', -- better text highlighting
 	'tpope/vim-fugitive', -- git
 	{ 'nvim-telescope/telescope.nvim', branch = '0.1.x', -- fuzzy finder
-		dependencies = { 'nvim-lua/plenary.nvim' } },
+		dependencies = { 'nvim-lua/plenary.nvim' }
+	},
 	{ 'rose-pine/neovim', name = 'rose-pine' }, -- color scheme
+	{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x',
+		dependencies = {
+			'neovim/nvim-lspconfig',
+			'hrsh7th/cmp-nvim-lsp',
+			'hrsh7th/nvim-cmp',
+			'L3MON4D3/LuaSnip',
+			'williamboman/mason.nvim',
+			'williamboman/mason-lspconfig.nvim',
+		}
+	}, -- language servers
 })
 
