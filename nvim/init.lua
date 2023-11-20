987f5be929709879af03cbbcab061e97745d812b
Robert Wallis <smilingrob@gmail.com>
2023-11-19 18:50:29 -0800

    moved <C-p> from git_files to find_files

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,5 @@
-require("robert")
 ----- default init ----
 -- set runtimepath^=~/.vim runtimepath+=~/.vim/after
 -- let &packpath = &runtimepath
 -- source ~/.vimrc
+require("robert")
--- nvim/lua/robert/remap.lua
+++ nvim/lua/robert/remap.lua
@@ -1,2 +1,3 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) -- in normal mode open netrw with " pv"
+vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
