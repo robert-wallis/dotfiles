1bddc1e74f9a4ee07039ca2c858ec61457ddfeb7
Robert Wallis <smilingrob@gmail.com>
2024-01-23 13:48:06 -0800

    default tabstop 4, smartcase

--- nvim/lua/robert/init.lua
+++ nvim/lua/robert/init.lua
@@ -2,3 +2,10 @@ require("robert.lazy")
 require("robert.colors")
 vim.cmd("set number relativenumber")
 vim.cmd[[let g:python3_host_prog = '/usr/local/bin/python3']]
+
+vim.softtabstop = 4
+vim.shiftwidth = 4
+vim.expandtab = true
+
+vim.ignorecase = true
+vim.smartcase = true -- only works when ignorecase is true, and search doesn't contain uppercase
