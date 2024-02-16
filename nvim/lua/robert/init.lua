56bae6bcf3a3f610a4ac28e9dfb6a16c8a8cfdd0
Robert Wallis <smilingrob@gmail.com>
2024-02-15 21:23:25 -0800

    lua var didn't mape to vim option cmd

--- nvim/lua/robert/init.lua
+++ nvim/lua/robert/init.lua
@@ -3,9 +3,9 @@ require("robert.colors")
 vim.cmd("set number relativenumber")
 vim.cmd[[let g:python3_host_prog = '/usr/local/bin/python3']]
 
-vim.softtabstop = 4
-vim.shiftwidth = 4
-vim.expandtab = true
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.cmd("set expandtab")
 
-vim.ignorecase = true
-vim.smartcase = true -- only works when ignorecase is true, and search doesn't contain uppercase
+vim.cmd("set ignorecase")
+vim.cmd("set smartcase")
