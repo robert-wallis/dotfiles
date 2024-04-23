1de6e0b4b92a9eab71935760dbd81f1ed5565e7f
Robert Wallis <smilingrob@gmail.com>
2024-04-23 14:31:12 -0700

    fix rust lsp

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,19 +1,20 @@
 local lsp_zero = require('lsp-zero')
 
-lsp_zero.on_attach(function(client, buf)
+lsp_zero.on_attach(function(_, buf)
 	-- see :help lsp-zero-keybindings to learn the available actions
 	lsp_zero.default_keymaps({buffer = buf})
 end)
 
 require("mason").setup({})
+
 require("mason-lspconfig").setup {
 	ensure_installed = {
 		"eslint", -- javascript
 		"gopls", -- go
 		"jedi_language_server", -- python
 		"lua_ls",
-		"rust_analyzer",
 		"tsserver", -- typescript
+                -- "rust_analyzer", DON'T install, mason uses an old one
 		"volar", -- vue
 		"yamlls",
 	},
@@ -22,6 +23,18 @@ require("mason-lspconfig").setup {
 	handlers = {
 		-- important to have handlers, or lspconfig won't set up correctly
 		lsp_zero.default_setup,
-
 	},
+}
+
+require("lspconfig").rust_analyzer.setup {
+    settings = {
+        ["rust-analyzer"] = {
+            cargo = {
+                extraArgs = {"--profile=rust-analyzer"},
+            },
+            checkOnSave = {
+                command = "clippy",
+            },
+        },
+    },
 }
