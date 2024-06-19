cb44650cf3f8c7525460d63346cf4acc0af58619
Robert Wallis <smilingrob@gmail.com>
2024-06-19 16:33:39 -0700

    new signing key

--- .zshrc.d/llvm.zsh
+++ .zshrc.d/llvm.zsh
@@ -1,7 +1,10 @@
-llvm_path=$(brew --prefix llvm@17)
+if type "brew" > /dev/null; then
+	llvm_path=$(brew --prefix llvm@17)
 
-if [ -d "$llvm_path" ]; then
-    llvm_bin="$llvm_path/bin"
-    export PATH="$llvm_bin:$PATH"
-    printf " llvm@17"
+	if [ -d "$llvm_path" ]; then
+		llvm_bin="$llvm_path/bin"
+		export PATH="$llvm_bin:$PATH"
+		printf " llvm@17"
+	fi
 fi
+
