dae48211221b6c9639d4414850eaac1934097a31
Robert Wallis <smilingrob@gmail.com>
2024-07-11 20:52:40 -0700

    gcloud update for new folder structure

--- .zshrc.d/func.zsh
+++ .zshrc.d/func.zsh
@@ -1,6 +1,6 @@
 dirname=$(dirname "${(%):-%x}")
 funcs_path="${dirname}/../funcs"
 if [ -f "${funcs_path}" ]; then
-    printf " \e[33m. funcs\e[0m"
+    printf " \e[33mfuncs\e[0m"
     source "${funcs_path}"
 fi
--- .zshrc.d/gcloud.zsh
+++ .zshrc.d/gcloud.zsh
@@ -1,11 +1,25 @@
+gdir="google-cloud-sdk"
+gdir_full="not-found"
+gcloud_p="path.zsh.inc"
+gcloud_comp_p="completion.zsh.inc"
 
-gcloud_p="google-cloud-sdk/path.zsh.inc"
-if [ -f "$HOME/$gcloud_p" ]; then 
-    printf " \e[33m. gcloud\e[0m"
-    . "$HOME/$gcloud_p"; 
+# find the path to where the google-cloud-sdk was unzipped
+# https://cloud.google.com/sdk/docs/install-sdk
+if [ -d "$HOME/$gdir" ]; then
+    gdir_full="$HOME/$gdir"
+fi
+if [ -d "$HOME/OpenSource/$gdir" ]; then
+    gdir_full="$HOME/OpenSource/$gdir"
 fi
 
-gcloud_comp_p="google-cloud-sdk/completion.zsh.inc"
-if [ -f "$HOME/$gcloud_comp_p" ]; then 
-    . "$HOME/$gcloud_comp_p"; 
+# check if the right file exists and source it
+
+if [ -f "$gdir_full/$gcloud_p" ]; then
+    printf " \e[33mgcloud\e[0m"
+    . "$gdir_full/$gcloud_p";
+fi
+
+if [ -f "$gdir_full/$gcloud_comp_p" ]; then
+    printf "\e[33mℹ\e[0m"
+    . "$gdir_full/$gcloud_comp_p";
 fi
