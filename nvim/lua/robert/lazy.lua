0da481947437abd0ae295087ea4894897039c3e9
Robert Wallis <smilingrob@gmail.com>
2024-10-04 18:22:20 -0700

    added lazyvim and clangd

--- nvim/lua/robert/lazy.lua
+++ nvim/lua/robert/lazy.lua
@@ -1,19 +1,25 @@
 require("lazy").setup({
-	'nvim-treesitter/nvim-treesitter', -- better text highlighting
-	'tpope/vim-fugitive', -- git
-	{ 'nvim-telescope/telescope.nvim', branch = '0.1.x', -- fuzzy finder
-		dependencies = { 'nvim-lua/plenary.nvim' }
+	"nvim-treesitter/nvim-treesitter", -- better text highlighting
+	"tpope/vim-fugitive", -- git
+	{
+		"nvim-telescope/telescope.nvim",
+		branch = "0.1.x", -- fuzzy finder
+		dependencies = { "nvim-lua/plenary.nvim" },
 	},
-	{ 'rose-pine/neovim', name = 'rose-pine' }, -- color scheme
-	{ 'VonHeikemen/lsp-zero.nvim', branch = 'v3.x',
+	{ "rose-pine/neovim", name = "rose-pine" }, -- color scheme
+	{
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v3.x",
 		dependencies = {
-			'neovim/nvim-lspconfig',
-			'hrsh7th/cmp-nvim-lsp',
-			'hrsh7th/nvim-cmp',
-			'L3MON4D3/LuaSnip',
-			'williamboman/mason.nvim',
-			'williamboman/mason-lspconfig.nvim',
-		}
+			"neovim/nvim-lspconfig",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/nvim-cmp",
+			"L3MON4D3/LuaSnip",
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+		},
 	}, -- language servers
+	"rhysd/vim-clang-format",
+	{ "LazyVim/LazyVim", import = "lazyvim.plugins" },
+	{ import = "lazyvim.plugins.extras.lang.clangd" },
 })
-
