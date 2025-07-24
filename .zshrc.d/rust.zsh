680f6cc73ee3fa1b00084398f5f54e3b47d43f5b
Robert Wallis <smilingrob@gmail.com>
2025-07-23 23:52:48 -0700

    brew rustup

--- .zshrc.d/rust.zsh
+++ .zshrc.d/rust.zsh
@@ -5,3 +5,11 @@ if [ -d $cargo_home ]; then
     printf " cargo"
 fi
 unset cargo_home
+
+rustup_prefix=$(brew --prefix rustup)
+if [ -d "$rustup_prefix/bin" ]; then
+  export PATH="$PATH:$rustup_prefix/bin"
+  printf " rustup"
+fi
+unset rustup_prefix
+
