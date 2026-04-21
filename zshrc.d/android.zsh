ba18c4983176cdaebfe1ec66524d18ca4924caf7
Robert Wallis <smilingrob@gmail.com>
2026-04-21 13:49:26 -0700

    android studio cli path

--- zshrc.d/android.zsh
+++ zshrc.d/android.zsh
@@ -5,6 +5,7 @@ if [ -d $ANDROID_HOME ]; then
     printf " android"
     export PATH="$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools"
 fi
+unset android_home_p
 
 android_ndk_p="ndk/21.0.6113669"
 ANDROID_NDK_HOME="$ANDROID_HOME/$android_ndk_p"
@@ -13,6 +14,12 @@ if [ -d $ANDROID_NDK_HOME ]; then
     printf " NDK"
     export PATH="$PATH:$ANDROID_NDK_HOME"
 fi
-
-unset android_home_p
 unset android_ndk_p
+
+# studio .
+android_studio_cli="/Applications/Android Studio.app/Contents/MacOS/"
+if [ -f "$android_studio_cli/studio" ]; then
+    printf " studio"
+    export PATH="$PATH:$android_studio_cli"
+fi
+unset android_studio_cli
