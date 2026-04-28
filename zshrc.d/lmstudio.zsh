2909c2e602abedce20a5c18b882ee8679f278ef0
Robert Wallis <smilingrob@gmail.com>
2026-04-28 13:12:53 -0700

    fix gopath accidentally put in lmstudio

--- zshrc.d/lmstudio.zsh
+++ zshrc.d/lmstudio.zsh
@@ -1,7 +1,6 @@
 # End of LM Studio CLI section
 lmstudio="$HOME/.lmstudio/bin"
 if [ -d $lmstudio ]; then
-    export GOPATH="$lmstudio"
     export PATH="$PATH:$lmstudio/bin"
     printf " lmstudio"
 fi
